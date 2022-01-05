0332106cfe41c0bf038ea6ae2bfbc3c04b8e7587
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-01-05 21:15:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace PowerOf2
+{
+    class Program
+    {
+        public void printPowerOfTwo()
+        {
+            int Num;
+            Console.WriteLine("Enter number till which you want to print power of 2 : ");
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            for (int i = 1; i <= Num; i++)
+            {
+                double Power = Math.Pow(2, i);
+                Console.WriteLine(Power);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.printPowerOfTwo();
+
+            Console.ReadLine();
+        }
+
+    }
+}
