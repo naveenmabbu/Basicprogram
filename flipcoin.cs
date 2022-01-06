a13880501b0634e3e7116330fc4633ad996cf63b
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-01-06 11:42:17 +0530

    FLIPCOIN PROGRAM

---
+++ flipcoin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flipcoin
+{
+    internal class Program
+    {
+        public void CalculatePercentage()
+        {
+            int Num;
+
+            Console.WriteLine("Enter the Number :");
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            int countofHeads = 0;
+            int countofTail = 0;
+            int i = 0;
+            while (i <= Num)
+            {
+
+                i++;
+
+                Random random = new();
+                int Flip = random.Next(0, 2);
+
+                if (Flip == 0)
+                {
+
+                    Console.WriteLine("Tails");
+                    countofTail++;
+                }
+
+                else if (Flip == 1)
+                {
+
+
+                    Console.WriteLine("Head");
+                    countofHeads++;
+                }
+
+                double PercentageofHeads = (countofHeads * 100 / Num);
+                Console.WriteLine("Percentage of Tail is = " + PercentageofHeads);
+
+                double PercentageOfTails = 100 - PercentageofHeads;
+                Console.WriteLine("Percentage of Head is = " + PercentageOfTails);
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program flipcoin = new();
+            flipcoin.CalculatePercentage();
+        }
+    }
+}
