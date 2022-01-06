1220e859af628e3a21397b1295eda2a8664fccd9
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-01-06 11:49:33 +0530

    HARMONIC NUMBER

---
+++ Harmonicprogram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonicNumber
+{
+    internal class Program
+    {
+         public void FindHarmonicSeries()
+        {
+            Console.WriteLine("Welcome to Harmonic series");
+
+            int Num;
+            Console.WriteLine("Enter Number till you want to print Harmonic series: ");
+            Num = Convert.ToInt32(Console.ReadLine());
+            double sumofHarmonicSeries = 0.0;
+
+            for (int i = 1; i <= Num; i++)
+            {
+
+                Console.Write("1/{0} +", i);
+                sumofHarmonicSeries = sumofHarmonicSeries + 1 / (double)i;
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("\nSum of series upto {0} terms : {1}\n", +Num, +sumofHarmonicSeries);
+            Console.ReadLine();
+        }
+
+        static void Main()
+        {
+            Program harmonic = new Program();
+            harmonic.FindHarmonicSeries();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Flipcoin;
+using HarmonicNumber;
