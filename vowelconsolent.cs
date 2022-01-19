73840633593bbedee5ee11e1015093ad02f81d03
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-01-19 17:46:14 +0530

    basic programs

---
+++ vowelconsolent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vowel_or_consonant
+{
+    internal class vowelconsolent
+    {
+        public static void findvowelsorConsonents()
+        {
+            char ch;
+
+            Console.WriteLine("Enter any character: ");
+            ch = Convert.ToChar(Console.ReadLine());
+
+
+            // Condition for vowel checking
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+
+                Console.WriteLine(ch + " is Vowel.");
+
+            }
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                Console.WriteLine(ch + " is Consonant.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
